aced1b64386c369357e15d583c48cdbcada1a20d
ayato-shitomi <sw.172.135.9.xxx.l62118102@gmail.com>
2023-02-17 02:27:51 -0500

    on Fri Feb 17 02:27:51 AM EST 2023

--- js/header.js
+++ js/header.js
@@ -1,8 +1,18 @@
-document.write('<h1 id="title">Beginners Sec</h1> \
-<div> \
-	<ul id="headerUrlList"> \
-		<li><a href="./index.html"> Home </a></li> \
-		<li><a href="./about.html"> About / Join </a></li> \
-		<li><a href="./blog.html"> News / Blog </a></li> \
-	</ul> \
-</div>');
+if (navigator.userAgentData && navigator.userAgentData.mobile) {
+	document.write('<h1 id="title">Beginners Sec</h1> \
+	<div> \
+		<ul id="headerUrlList"> \
+			<li><a href="./about.html"> About / Join </a></li> \
+		</ul> \
+	</div> \
+	<h3>一部動作はPCのみ対応します。</h3>');
+	} else {
+	document.write('<h1 id="title">Beginners Sec</h1> \
+	<div> \
+		<ul id="headerUrlList"> \
+			<li><a href="./index.html"> Home </a></li> \
+			<li><a href="./about.html"> About / Join </a></li> \
+			<li><a href="https://qiita.com/organizations/BeginnersSec" target="_blank"> News / Blog </a></li> \
+		</ul> \
+	</div>');
+}
